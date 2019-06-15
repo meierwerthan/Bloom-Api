0319c7a892a9a22499c85a55b26f55f738e89637
meierwerthan <meier.werthan@du.edu>
2019-06-14 19:16:01 -0600

    fuckme

--- src/PaymentStripe/frontend/constants/server.js
+++ src/PaymentStripe/frontend/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production' //Setting The back-end server url's
   ? 'https://bloom-userui.herokuapp.com'
-  : 'http://localhost:80';
+  : 'http://localhost:8080';
 
 export default PAYMENT_SERVER_URL;
